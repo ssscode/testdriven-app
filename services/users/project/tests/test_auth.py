d1e0ab41b3c53ce60fd9fdd3c1d43bda0c3b3a32
ssscode <sashshok@gmail.com>
2018-12-26 12:18:13 +0200

    Upd docker and travis files

---
+++ services/users/project/tests/test_auth.py
@@ -0,0 +1,10 @@
+import json
+
+from project import db
+from project.api.models import User
+from project.tests.base import BaseTestCase
+from project.tests.utils import add_user
+
+
+class TestAuthBlueprint(BaseTestCase):
+    pass
